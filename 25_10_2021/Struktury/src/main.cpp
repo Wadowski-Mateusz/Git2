ece31fe23847d804c28a4d73bd437a48eafc9554
Wadowski-Mateusz <mateusz.wadow@gmail.com>
2021-10-25 12:33:51 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	simpleCalc

--- 25_10_2021/Struktury/src/main.cpp
+++ 25_10_2021/Struktury/src/main.cpp
@@ -6,7 +6,7 @@
 
 using namespace std;
 
-struct samochod{
+struct samochod {
 
     string marka;
     string model;
@@ -14,19 +14,90 @@ struct samochod{
     string kolor;
     bool bity;
 
+    void printSamochod() {
+        cout << marka << "\t|\t" << model << "\t|\t" << rokProdukcji << "\t|\t" << kolor << "\t|\t" << bity << endl;
+    }
+
 };
 
-int main(){
+int marka(samochod *a, int no);
+
+int najstarszy(samochod *a, int no);
+
+int main() {
 
     static const int no = 4;
     samochod *a = new samochod[no];
 
     a[0].marka = "marka1";
     a[0].model = "model1";
-    a[0].rokProdukcji = 0;
-    a[0].kolor = "kolor";
+    a[0].rokProdukcji = 1;
+    a[0].kolor = "kolor1";
     a[0].bity = 0;
+
+    a[1].marka = "marka2";
+    a[1].model = "model2";
+    a[1].rokProdukcji = 2;
+    a[1].kolor = "kolor2";
+    a[1].bity = 1;
+
+    a[2].marka = "marka1";
+    a[2].model = "model3";
+    a[2].rokProdukcji = 3;
+    a[2].kolor = "kolor3";
+    a[2].bity = 0;
+
+    a[3].marka = "marka4";
+    a[3].model = "model4";
+    a[3].rokProdukcji = -100;
+    a[3].kolor = "kolor4";
+    a[3].bity = 1;
+
+
+    cout
+            << "marka\t|\tmodel\t| rok Produkcji |\tkolor\t|\tbity\n______________________________________________________________________\n";
+    a[0].printSamochod();
+    a[1].printSamochod();
+    a[2].printSamochod();
+    a[3].printSamochod();
+
+
+    cout << "\n\n\n\n\n";
+
+    int m = marka (a, no);
+    cout << "Liczba samochodow o takiej marce:" << m << endl;
+
+    cout << "\n\n\n\n\n";
+
+    cout << "Najstarszy samochod znajduje sie pod indeksem: " << najstarszy(a, no) << endl;
+
 
     delete[] a;
     return 0;
+}
+
+int marka(samochod *a, int no) {
+    int index;
+
+    //Napisz funkcję, która będzie przyjmować tablicę samochodów oraz liczbę elementów w tablicy i zwróci liczbę(sumę) samochodów, które są tej samej marki.
+    //porownywanie do 1. elementu tablicy;
+    string szukaj;
+    cout << "Podaj szukana marke\n";
+    cin >> szukaj;
+    int no2 = 0;
+
+    for (int i = 0; i < no; i++)
+        if (a[i].marka == szukaj)
+            no2++;
+
+    return no2;
+}
+
+int najstarszy(samochod *a, int no) {
+    int min = 0;
+
+    for (int i = 1; i < no; i++)
+        if (a[i].rokProdukcji < a[min].rokProdukcji)
+            min = i;
+    return min;
 }
---
+++ 25_10_2021/klasy_i_obiekty/src/main.cpp
@@ -0,0 +1,19 @@
+//
+// Created by mateu on 25.10.2021.
+//
+
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    return 0;
+}
+
+
+/*
+Napisz program, który będzie liczył pole powierzchni prostopadłościanu(klasa Prostopadłościan z trzema polami).
+Napisz program obliczający objętość kuli (klasa Kula o polu r).
+Napisz program, który dla wprowadzonych współczynników funkcji kwadratowej wyświetli wprowadzone dane w postaci: f(x) = a x^2 + b x + c (klasa FunckjaKwadratowa o polach a, b,c).
+Napisz program, ktory stworzy klasę Student o polach: imię, nazwisko, numer albumu, liczba pytan, poprawne odpowiedzi. Napisz funckję, która wczyta te dane oraz funkcję, która obliczy procent poprawnych odpowiedzi.
+*/
